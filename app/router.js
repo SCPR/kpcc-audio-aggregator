a91af32f866468abe77fedab9659af08416cd2e8
Sean Dillingham <sean.dillingham@gmail.com>
2014-07-05 16:07:09 -0700

    initialize ember app

---
+++ app/router.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+var Router = Ember.Router.extend({
+  location: KpccAudioAggregatorENV.locationType
+});
+
+Router.map(function() {
+});
+
+export default Router;
---
+++ tests/helpers/resolver.js
@@ -0,0 +1,9 @@
+import Resolver from 'ember/resolver';
+
+var resolver = Resolver.create();
+
+resolver.namespace = {
+  modulePrefix: 'kpcc-audio-aggregator'
+};
+
+export default resolver;
