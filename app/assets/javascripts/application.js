827d61e512a1e60f2d4f136327c3335b937b0618
John Meeker <jmeeker516@gmail.com>
2014-07-09 11:18:18 -0700

    Convert to plain ol' JavaScript. Re-bootstrap with appropriate Ember files.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,14 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require jquery
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require_self
+//= require ./kpcc_audio_aggregator
+
+// for more details see: http://emberjs.com/guides/application/
+KpccAudioAggregator = Ember.Application.create();
+
 //= require_tree .
--- app/assets/javascripts/router.js.coffee
+++
@@ -1,5 +0,0 @@
-# For more information see: http://emberjs.com/guides/routing/
-
-KpccAudioAggregator.Router.map ()->
-  # @resource('posts')
-
