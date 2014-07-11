fe63de2cbbb094e756367010f18111d44fb45c48
John Meeker <jmeeker516@gmail.com>
2014-07-11 16:22:49 -0700

    Convert to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140711220403) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "audio_stories", force: true do |t|
     t.string   "category"
     t.string   "title"
