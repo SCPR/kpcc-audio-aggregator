73b4015ac6d4c006048e8ce1291e46db7b69a0d3
John Meeker <jmeeker516@gmail.com>
2014-07-18 10:29:28 -0700

    Add importer for Morning Edition segments.

--- app/importers/kpcc_program_importer.rb
+++ app/importers/kpcc_program_importer.rb
@@ -1,5 +1,7 @@
 module KpccProgramImporter
 
+  NPR_SOURCE = "NPR"
+
   class << self
 
     def sync_all_things_considered
@@ -10,11 +12,11 @@ module KpccProgramImporter
       program_hash['episodes'].each do |episode|
 
         episode['segments'].reject { |s|
-          AudioStory.exists?(source: "NPR", article_id:s['id']) || s['audio'].count == 0
+          AudioStory.exists?(source: NPR_SOURCE, article_id:s['id']) || s['audio'].count == 0
         }.each do |episode_segment|
-    
+
           audio_story = AudioStory.new(
-            :source       => "NPR",
+            :source       => NPR_SOURCE,
             :program      => "All Things Considered",
             :article_id   => episode_segment['id'],
             :url          => episode_segment['public_url'],
@@ -23,7 +25,42 @@ module KpccProgramImporter
             :duration     => episode_segment['audio'].first['duration'],
             :time_stamp   => episode_segment['audio'].first['uploaded_at']
           )
-  
+
+          if audio_story.save
+            added.push audio_story
+            puts "AudioStory saved: #{audio_story.title}"
+          else
+            puts "Failed to save AudioStory"
+          end
+        end # each segment
+
+        added
+      end #each episode
+    end
+
+
+    def sync_morning_edition
+      program_hash = KpccApiHelper::Episodes.morning_edition
+
+      added = []
+
+      program_hash['episodes'].each do |episode|
+
+        episode['segments'].reject { |s|
+          AudioStory.exists?(source: NPR_SOURCE, article_id:s['id']) || s['audio'].count == 0
+        }.each do |episode_segment|
+
+          audio_story = AudioStory.new(
+            :source       => NPR_SOURCE,
+            :program      => "Morning Edition",
+            :article_id   => episode_segment['id'],
+            :url          => episode_segment['public_url'],
+            :title        => episode_segment['title'],
+            :audio_url    => episode_segment['audio'].first['url'],
+            :duration     => episode_segment['audio'].first['duration'],
+            :time_stamp   => episode_segment['audio'].first['uploaded_at']
+          )
+
           if audio_story.save
             added.push audio_story
             puts "AudioStory saved: #{audio_story.title}"
