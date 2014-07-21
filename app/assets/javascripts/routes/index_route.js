c0043901a6cce7a41d31227b3829e897ee954d00
John Meeker <jmeeker516@gmail.com>
2014-07-21 14:53:15 -0700

    Hook up audio_collection_new modal to Ember actions for open and close.

--- app/assets/javascripts/routes/index_route.js
+++ app/assets/javascripts/routes/index_route.js
@@ -4,5 +4,20 @@ KpccAudioAggregator.IndexRoute = Ember.Route.extend({
       audioStories: this.store.find('audio_story'),
       audioCollections: this.store.find('audio_collection')
     });
+  },
+
+  actions: {
+    openModal: function(modalName) {
+      return this.render(modalName, {
+        into: 'application',
+        outlet: 'modal'
+      });
+    },
+    closeModal: function() {
+      return this.disconnectOutlet({
+        outlet: 'modal',
+        parentView: 'application'
+      });
+    }
   }
 });
---
+++ app/assets/javascripts/views/audio_collection_new_view.js
@@ -0,0 +1,5 @@
+// for more details see: http://emberjs.com/guides/views/
+
+KpccAudioAggregator.AudioCollectionNewView = Ember.View.extend({
+  templateName: 'audio_collection_new'
+});
