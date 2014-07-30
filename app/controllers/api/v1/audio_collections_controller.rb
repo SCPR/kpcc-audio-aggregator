8d6200519c8bf4d40b049f8812d20516c9770f65
John Meeker <jmeeker516@gmail.com>
2014-07-30 15:16:11 -0700

    Fix AudioCollection create.. update params in api.

--- app/controllers/api/v1/audio_collections_controller.rb
+++ app/controllers/api/v1/audio_collections_controller.rb
@@ -46,6 +46,7 @@ class Api::V1::AudioCollectionsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def audio_collection_params
-    params.require(:audio_collection).permit(:title, :status, :updated_at, :audio_story_ids, :audio_stories)
+    params[:audio_collection][:audio_stories] ||= [] if params[:audio_collection].has_key?(:audio_stories)
+    params.require(:audio_collection).permit(:title, :status, :updated_at, :audio_story_ids, :audio_stories, audio_story_ids: [], audio_stories: [])
   end
 end
